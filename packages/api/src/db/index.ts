cd746057f21662009addb23e1d989d88a3261f96
George Borg <george@noquarter.co>
2019-08-08 17:50:57 +0200

    config

--- packages/api/src/db/index.ts
+++ packages/api/src/db/index.ts
@@ -1,17 +1,17 @@
-import { getConnectionOptions, createConnection } from "typeorm"
-import { NODE_ENV, DATABASE_URL, isProduction } from "../lib/config"
+import { getConnectionOptions } from "typeorm"
+import { NODE_ENV } from "../lib/config"
 
 export const createDbConnection = async () => {
   // Create DB connection
-  const options = await getConnectionOptions(NODE_ENV)
+  await getConnectionOptions(NODE_ENV)
 
-  const connection = await createConnection({
-    ...options,
-    name: "default",
-    // @ts-ignore
-    url: DATABASE_URL,
-  })
+  // const connection = await createConnection({
+  //   ...options,
+  //   name: "default",
+  //   // @ts-ignore
+  //   url: DATABASE_URL,
+  // })
 
   // Run migrations in production
-  if (isProduction) await connection.runMigrations()
+  // if (isProduction) await connection.runMigrations()
 }
