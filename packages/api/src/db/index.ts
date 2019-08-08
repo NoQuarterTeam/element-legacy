2f596ac3697be8f91d690a6d7e2dfe9c06348a5a
George Borg <george@noquarter.co>
2019-08-08 17:55:25 +0200

    fix typeorm connection

--- packages/api/src/db/index.ts
+++ packages/api/src/db/index.ts
@@ -1,16 +1,16 @@
-import { getConnectionOptions } from "typeorm"
+import { createConnection, getConnectionOptions } from "typeorm"
 import { NODE_ENV } from "../lib/config"
 
 export const createDbConnection = async () => {
   // Create DB connection
-  await getConnectionOptions(NODE_ENV)
+  const options = await getConnectionOptions(NODE_ENV)
 
-  // const connection = await createConnection({
-  //   ...options,
-  //   name: "default",
-  //   // @ts-ignore
-  //   url: DATABASE_URL,
-  // })
+  await createConnection({
+    ...options,
+    name: "default",
+    // @ts-ignore
+    url: DATABASE_URL,
+  })
 
   // Run migrations in production
   // if (isProduction) await connection.runMigrations()
