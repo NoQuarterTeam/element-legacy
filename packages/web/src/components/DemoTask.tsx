999003fa3a4f64c97ff4bb15e8b7b0ebda5326d0
Mark van der Laan <mrkvdl@gmail.com>
2019-12-13 17:14:01 +0100

    DemoTask to chakra UI

--- packages/web/src/components/DemoTask.tsx
+++ packages/web/src/components/DemoTask.tsx
@@ -1,20 +1,20 @@
 import React, { memo } from "react"
-import styled from "styled-components"
-import { darken, readableColor, lighten } from "polished"
+
+import { Text } from "@chakra-ui/core"
+import { styled } from "./providers/ThemeProvider"
 
 interface Props {
   task: {
     name: string
     completed: boolean
     element: { color: string; name: string }
-    startTime: string //TODO: DateTime?
-    estimatedTime: string //TODO: DateTime?
+    startTime: string
+    estimatedTime: string
   }
   isDragging: boolean
   hidden: boolean
 }
 
-// TODO: Make it Chakralicious
 function DemoTask({ task, hidden, ...rest }: Props) {
   return (
     <>
@@ -29,11 +29,17 @@ function DemoTask({ task, hidden, ...rest }: Props) {
         <StyledTaskElement
           completed={task.completed}
           color={task.element.color}
+          fontSize="sm"
         >
           {task.element.name}
         </StyledTaskElement>
         {task.startTime && (
-          <StyledTaskStart completed={task.completed}>
+          <StyledTaskStart
+            completed={task.completed}
+            fontSize="sm"
+            bottom={6}
+            right={1}
+          >
             @{task.startTime}
           </StyledTaskStart>
         )}
@@ -47,9 +53,8 @@ function DemoTask({ task, hidden, ...rest }: Props) {
 
 export default memo(DemoTask)
 
-const StyledTaskElement = styled.p<{ completed: boolean; color: string }>`
+const StyledTaskElement = styled(Text)<{ completed: boolean; color: string }>`
   position: absolute;
-  font-size: ${p => p.theme.textS};
   top: 0;
   right: 0;
   margin: 0;
@@ -63,33 +68,27 @@ const StyledTaskElement = styled.p<{ completed: boolean; color: string }>`
   color: ${p => p.color};
   transition: width 0.1s ease-out;
   text-align: center;
-  border-radius: ${p => `0 ${p.theme.borderRadius} 0 0`};
+  border-radius: 0 ${p => p.theme.borders["4px"]} 0 0;
 `
 
-const StyledTaskStart = styled.p<{ completed: boolean }>`
-  color: ${p => (p.completed ? "lightgrey" : p.theme.colorText)};
+const StyledTaskStart = styled(Text)<{ completed: boolean }>`
+  color: ${p => (p.completed ? "lightgrey" : p.theme.colors.blackAlpha[700])};
   position: absolute;
-  font-size: ${p => p.theme.textS};
-  bottom: ${p => p.theme.paddingML};
-  right: ${p => p.theme.paddingXS};
   margin: 0;
-  /* text-decoration: ${p => (p.completed ? "line-through" : "none")}; */
 `
 
-const StyledTaskDuration = styled.p<{ completed: boolean }>`
+const StyledTaskDuration = styled(Text)<{ completed: boolean }>`
   position: absolute;
-  bottom: ${p => p.theme.paddingXS};
-  right: ${p => p.theme.paddingXS};
-  color: ${p => (p.completed ? "lightgrey" : p.theme.colorText)};
-  font-size: ${p => p.theme.textS};
   margin: 0;
-  /* text-decoration: ${p => (p.completed ? "line-through" : "none")}; */
+  color: ${p => (p.completed ? "lightgrey" : p.theme.colors.blackAlpha[700])};
+  bottom: ${p => p.theme.sizes[1]};
+  right: ${p => p.theme.sizes[1]};
+  font-size: ${p => p.theme.fontSizes.sm};
 `
 
-const StyledTaskName = styled.p<{ completed: boolean }>`
-  color: ${p => (p.completed ? "lightgrey" : p.theme.colorText)};
-  font-size: ${p => p.theme.textS};
-  /* text-decoration: ${p => (p.completed ? "line-through" : "none")}; */
+const StyledTaskName = styled(Text)<{ completed: boolean }>`
+  color: ${p => (p.completed ? "lightgrey" : p.theme.colors.blackAlpha[700])};
+  font-size: ${p => p.theme.fontSizes.sm};
   overflow: hidden;
   max-height: 26px;
   line-height: 13px;
@@ -103,28 +102,14 @@ const StyledTaskBox = styled.div<{
 }>`
   display: ${p => (p.hidden ? "none" : "flex")};
   position: relative;
-  cursor: pointer;
   flex-direction: column;
   width: 140px;
   min-height: 80px;
   background-color: white;
-  margin: ${p => p.theme.paddingM} ${p => p.theme.paddingS};
-  padding: ${p => p.theme.paddingS};
-  box-shadow: ${p => (p.completed ? p.theme.boxShadow : p.theme.boxShadow)};
+  margin: ${p => p.theme.sizes[4]} ${p => p.theme.sizes[2]};
+  padding: ${p => p.theme.sizes[2]};
+  box-shadow: ${p => p.theme.shadows.md};
   filter: ${p => (p.completed ? "blur(0.6px)" : null)};
   opacity: ${p => (p.completed ? 0.8 : 1)};
-  border-radius: ${p => p.theme.borderRadius};
-
-  &:hover ${StyledTaskElement} {
-    height: 25px;
-    width: 100%;
-    text-overflow: ellipsis;
-    padding: ${p => p.theme.paddingXS};
-    color: ${p =>
-      readableColor(p.color, darken(0.3, p.color), lighten(0.35, p.color))};
-    border-radius: ${p => p.theme.borderRadius} ${p => p.theme.borderRadius} 0 0;
-  }
-  &:hover ${StyledTaskName} {
-    display: none;
-  }
+  border-radius: ${p => p.theme.borders["4px"]};
 `
